e2ba32f80edb5e710ddc2cde78da483c9e300c77
Charles Finkel <charles.finkel@gmail.com>
2011-05-24 16:50:58 -0700

    test for app.rb

--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -8,16 +8,16 @@ describe App do
 
   describe 'signals' do
     describe "key-release-event" do
-      it "should change search_text to value of text entry" do
+      context 'when Ctrl-L is pressed' do
+        before do
+          @keyval = Gdk::Keyval::GDK_L
+        end
+        it "should highlight text entry" do
+          @app.text_edit_view.focus = true
 
-        main_accel_group = @app.instance_variable_get(:@main_accel_group)
-        main_accel_group
-=begin
-        @app.search_text_entry.text = 'blady'
-
-        expect { @app.search_text_entry.signal_emit("key-release-event", nil) }.
-            to change { @app.search_text }.from(nil).to('blady')
-=end
+          expect { Gtk::AccelGroup.activate(@app.window, @keyval, Gdk::Window::CONTROL_MASK) }.to
+          change { @app.search_text_entry.focus? }.from(false).to(true)
+        end
       end
     end
 
