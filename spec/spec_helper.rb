64e4cc869ecb033ff01d652d622391529be42959
Charles Finkel <charles.finkel@gmail.com>
2011-06-17 11:57:47 -0700

    optmizing specs a bit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,19 +6,14 @@ require 'factories'
 RSpec.configure do |config|
   config.mock_with :rspec
 
-  config.before(:all) do
-    ActiveRecord::Base.establish_connection(App.database_config)
-
-    CreateNotes.down
-    CreateNotes.up
-  end
-
   config.before(:each) do
     Note.destroy_all
   end
 end
 
 ActiveRecord::Base.establish_connection(App.database_config)
+CreateNotes.down
+CreateNotes.up
 
 VCR.config do |c|
   c.cassette_library_dir = File.join(File.expand_path('..', __FILE__), 'fixtures', 'vcr_cassettes')
